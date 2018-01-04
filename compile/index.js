65c94a4058f926c1f2a43abfd20b6df510ce29f6
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-01-04 23:11:08 +0300

    Add parse phase Add translate phase

--- compile/index.js
+++ compile/index.js
@@ -1,11 +1,19 @@
+const parse = require("./parse");
 const prepare = require("./prepare");
 const print = require("./print");
 const read = require("./read");
+const translate = require("./translate");
 
 const printReadResult = print(1, "read lines from source code");
 const printPrepareResult = print(2, "prepare code tree from lines");
+const printParseResult = print(3, "parse prepared code tree");
+const printTranslateResult = print(4, "translate parsed code tree to JS");
 
 read
   .then(printReadResult)
   .then(prepare)
-  .then(printPrepareResult);
+  .then(printPrepareResult)
+  .then(parse)
+  .then(printParseResult)
+  .then(translate)
+  .then(printTranslateResult);
---
+++ compile/parse/index.js
@@ -0,0 +1,20 @@
+const _ = require("lodash");
+
+const parseLine = line => {
+  const parts = line.line.split(" = ");
+  const name = parts[0].trim();
+  const value = parts[1].trim();
+
+  return { type: "declare", name, value };
+};
+
+const parse = lines => {
+  if (lines.length === 0) return [];
+
+  const parsedLine = parseLine(_.head(lines));
+  const parsedTail = parse(_.tail(lines));
+
+  return [parsedLine].concat(parsedTail);
+};
+
+module.exports = parse;
