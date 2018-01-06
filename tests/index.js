0d2c527ca61dd1baaf98ed7667cd19c5c233a91f
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-01-06 21:35:07 +0300

    Add tests for string, float and boolean declarations

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,26 @@
 module.exports = {
-  "Integer declarations": {
+  "Integer declaration": {
     "should be compiled well": {
-      input: "apples = 1",
-      output: "const apples = 1;"
+      input: `count = 1`,
+      output: `const count = 1;`
+    }
+  },
+  "Float declaration": {
+    "should be compiled well": {
+      input: `count = 1.5`,
+      output: `const count = 1.5;`
+    }
+  },
+  "String declaration": {
+    "should be compiled well": {
+      input: `name = 'John'`,
+      output: `const name = 'John';`
+    }
+  },
+  "Boolean declaration": {
+    "should be compiled well": {
+      input: `visible = true`,
+      output: `const visible = true;`
     }
   }
 };
