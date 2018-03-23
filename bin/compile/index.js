89114b53c350fa00bef1559279ac59517739373f
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2018-03-23 12:34:00 +0300

    Add test infrastructure

--- bin/compile/index.js
+++ bin/compile/index.js
@@ -5,7 +5,7 @@ const translate = require('./translate')
 module.exports = input => {
   const tree = parse(input)
   const translated = tree.map(translate).join('\n')
-  const header = 'require("sova-standard-library");\n\n'
+  const header = 'require("sova-standard-library")\n\n'
   const complete = `${header}${translated}`
   const formatted = prettier.format(complete, {
     printWidth: 120,
--- test/index.js
+++ test/index.js
@@ -1,1 +1,31 @@
-const read = require('../bin/io/read')
+const assert = require('chai').assert
+const compile = require('../bin/compile')
+const FileSystem = require('fs')
+const testsDirectory = './test/'
+
+const print = console.log
+
+FileSystem.readdirSync(testsDirectory)
+  .map(file => `${testsDirectory}${file}`)
+  .filter(file => FileSystem.lstatSync(file).isDirectory())
+  .map(directory =>
+    describe(directory.substring(testsDirectory.length, directory.length), () => {
+      const [input, output, result] = ['input.sv', 'output.js', 'result.txt']
+        .map(name => `${directory}/${name}`)
+        .map(file => FileSystem.readFileSync(file, 'utf8'))
+
+      const compiled = compile(input)
+      it('compiles', () => assert.equal(compiled, output))
+
+      const requireHeader = `require("sova-standard-library")`
+      const clean = compiled.substring(compiled.length, requireHeader.length)
+
+      const expectedLogs = result.split('\n').filter(line => line)
+      const logs = []
+      console.log = value => logs.push(value)
+
+      eval(clean)
+      console.log = print
+      it('runs', () => assert.deepEqual(logs, expectedLogs))
+    })
+  )
