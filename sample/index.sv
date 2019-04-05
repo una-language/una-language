b5e4714a613efb206878a195cb69dcf5d2ae277c
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2019-04-05 23:13:52 +0300

    Add function

--- sample/index.sv
+++ sample/index.sv
@@ -1,5 +1,7 @@
 = checkNumber -> number
-  ? (=== number 1) <- 'One'
-  ? (=== number 2) <- 'Two'
-  ? (<= number 9) <- 'From three to nine'
-  'Ten or more'
+  ? (=== number 1) (<- 'One')
+  ? (=== number 2) (<- 'Two')
+  ? (<= number 9) (<- 'From three to nine')
+  <- 'Ten or more'
+
+console.log (checkNumber 6)
--- src/compile/translate.js
+++ src/compile/translate.js
@@ -12,6 +12,23 @@ const expression = ast => {
     return application(head, ...tail)
 }
 
+const func = (parameters, ...lines) => {
+    const last = lines[lines.length - 1]
+    const lastTranslated = Array.isArray(last) && last[0] === '<-' ? expression(last) : `return ${expression(last)}`
+    const params = Array.isArray(parameters)
+        ? parameters.length
+            ? parameters.map(expression).join(', ')
+            : []
+        : [expression(parameters)]
+    const body = lines
+        .slice(0, -1)
+        .map(expression)
+        .map(line => `${line};`)
+        .join(' ')
+
+    return `(${params}) => {${body} ${lastTranslated};}`
+}
+
 const nary = symbol => (...parameters) => `(${parameters.map(expression).join(` ${symbol} `)})`
 const unary = symbol => parameter => `(${symbol}${expression(parameter)})`
 
@@ -25,11 +42,11 @@ const languageConstructions = {
     '|': (...elements) => `[${elements.map(expression).join(', ')}]`,
     ':': () => null, // map
 
-    '->': () => null, // function (check that last line is return but can already have <- sign)
-    '<-': returningValue => `return ${expression(returningValue)}`,
+    '->': func, // function (check that last line is return but can already have <- sign)
+    '<-': (...returningValue) => `return ${expression(returningValue)}`,
 
-    '-->': () => null, // async function
-    '<--': () => null, // await
+    '-->': (...parameters) => `async ${func(...parameters)}`, // async function
+    '<--': (...parameters) => `await ${expression(parameters)}`, // await
 
     '=->': () => null, // require
     '<-=': exportedValue => `module.exports = ${expression(exportedValue)}`,
