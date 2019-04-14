cdc0bf843e4646cbec16b06ce9de13da05e22a9e
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2019-04-14 21:43:50 +0300

    Add more examples

--- example/console/src/index.sv
+++ example/console/src/index.sv
@@ -1,4 +1,19 @@
+console.log 'Constant declaration'
+console.log '-----------'
+=-> './constant-declaration'
+console.log '-----------\n'
+
+console.log 'Numbers'
+console.log '-----------'
+=-> './numbers'
+console.log '-----------\n'
+
+console.log 'Comparison'
+console.log '-----------'
+=-> './comparison'
+console.log '-----------\n'
+
 console.log 'Average age'
 console.log '-----------'
 =-> './average-age'
-console.log '-----------'
+console.log '-----------\n'
