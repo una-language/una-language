b70a13268490cb3e095a862305ccc3ee158e8451
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2020-09-10 17:11:25 +0300

    Start writing functions and tests for them

--- src-new/translate.js
+++ src-new/translate.js
@@ -1,5 +1,12 @@
 const expression = node => {
-    if (node.type === '=') return `const ${expression(node.left)} = ${expression(node.right)}`
+    switch (node.type) {
+        case '=':
+            return `const ${expression(node.left)} = ${expression(node.right)}`
+        case '+':
+            return node.params.map(expression).join(' + ')
+        case '-':
+            return node.params.length > 1 ? node.params.map(expression).join(' - ') : `-${node.params[0]}`
+    }
 
     return node
 }
--- src-new/translate.test.js
+++ src-new/translate.test.js
@@ -2,6 +2,15 @@ const translate = require('./translate')
 
 const testTranslate = (tree, js) => expect(translate(tree).trim()).toEqual(js.trim())
 
-test('Assignment', () => {
+test('=', () => {
     testTranslate({ type: '=', left: 'a', right: '1' }, 'const a = 1')
+})
+
+test('+', () => {
+    testTranslate({ type: '+', params: ['1', '2'] }, '1 + 2')
+})
+
+test('-', () => {
+    testTranslate({ type: '-', params: ['1'] }, '-1')
+    testTranslate({ type: '-', params: ['2', '1'] }, '2 - 1')
 })
