113813ab769f00b8bb0ad7cbbe97147fde302bd4
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2020-09-28 16:42:15 +0300

    Make transform work right

--- packages/una-language/src/transform.js
+++ packages/una-language/src/transform.js
@@ -5,12 +5,12 @@ const transform = raw => {
     if (!Array.isArray(raw)) return raw
     if (raw.length === 1) return transform(raw[0])
 
-    const [operator, ...params] = raw
+    const [value, ...children] = raw
 
-    if (['=', '<-='].includes(operator))
-        return { type: operator, children: [transform(params[0]), transform(params.slice(1))] }
-    if (['<--', '=->'].includes(operator)) return { type: operator, children: [transform(params)] }
-    return { type: operator, children: params.map(transform) }
+    if (['=', '<-='].includes(value))
+        return [value, transform(children[0]), transform(children.slice(1))]
+    if (['<--', '=->'].includes(value)) return [value, transform(children)]
+    return raw.map(transform)
 }
 
 module.exports = transform
--- packages/una-language/src/transform.test.js
+++ packages/una-language/src/transform.test.js
@@ -2,62 +2,29 @@ const transform = require('./transform')
 const testTransform = (raw, tree) => expect(transform(raw)).toEqual(tree)
 
 test('=', () => {
-    testTransform(['=', 'a', 1], { type: '=', children: ['a', 1] })
-    testTransform(['=', 'a', ['+', 1, 2]], {
-        type: '=',
-        children: ['a', { type: '+', children: [1, 2] }]
-    })
-    testTransform(['=', 'a', '+', 1, 2], {
-        type: '=',
-        children: ['a', { type: '+', children: [1, 2] }]
-    })
+    testTransform(['=', 'a', '1'], ['=', 'a', '1'])
+    testTransform(['=', 'a', ['+', '1', '2']], ['=', 'a', ['+', '1', '2']])
+    testTransform(['=', 'a', '+', '1', '2'], ['=', 'a', ['+', '1', '2']])
 })
 
 test('+', () => {
-    testTransform(['+', 1, 2], { type: '+', children: [1, 2] })
+    testTransform(['+', '1', '2'], ['+', '1', '2'])
 })
 
 test('<--', () => {
-    testTransform(['=', 'a', ['<--', ['promise']]], {
-        type: '=',
-        children: ['a', { type: '<--', children: ['promise'] }]
-    })
-    testTransform(['=', 'a', ['<--', 'promise']], {
-        type: '=',
-        children: ['a', { type: '<--', children: ['promise'] }]
-    })
-    testTransform(['=', 'a', '<--', 'promise'], {
-        type: '=',
-        children: ['a', { type: '<--', children: ['promise'] }]
-    })
+    testTransform(['=', 'a', ['<--', ['promise']]], ['=', 'a', ['<--', 'promise']])
+    testTransform(['=', 'a', ['<--', 'promise']], ['=', 'a', ['<--', 'promise']])
+    testTransform(['=', 'a', '<--', 'promise'], ['=', 'a', ['<--', 'promise']])
 })
 
 test('=->', () => {
-    testTransform(['=->', 'a'], {
-        type: '=->',
-        children: ['a']
-    })
-    testTransform(['=->', ['+', 1, 2]], {
-        type: '=->',
-        children: [{ type: '+', children: [1, 2] }]
-    })
-    testTransform(['=->', '+', 1, 2], {
-        type: '=->',
-        children: [{ type: '+', children: [1, 2] }]
-    })
+    testTransform(['=->', 'a'], ['=->', 'a'])
+    testTransform(['=->', ['+', '1', '2']], ['=->', ['+', '1', '2']])
+    testTransform(['=->', '+', '1', '2'], ['=->', ['+', '1', '2']])
 })
 
 test('<-=', () => {
-    testTransform(['<-=', "'A'", 'a'], {
-        type: '<-=',
-        children: ["'A'", 'a']
-    })
-    testTransform(['<-=', "'A'", [':', 'a']], {
-        type: '<-=',
-        children: ["'A'", { type: ':', children: ['a'] }]
-    })
-    testTransform(['<-=', "'A'", ':', 'a'], {
-        type: '<-=',
-        children: ["'A'", { type: ':', children: ['a'] }]
-    })
+    testTransform(['<-=', '"A"', 'a'], ['<-=', '"A"', 'a'])
+    testTransform(['<-=', '"A"', [':', 'a']], ['<-=', '"A"', [':', 'a']])
+    testTransform(['<-=', '"A"', ':', 'a'], ['<-=', '"A"', [':', 'a']])
 })
