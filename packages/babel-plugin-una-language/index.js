3bfeca6d1dddc2cfcbb2bfb5f4ae5f744e3a82f0
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-03 23:36:39 +0300

    Move babel plugin to another repo

--- packages/babel-plugin-una-language/index.js
+++
@@ -1,7 +0,0 @@
-const parser = require('@babel/parser')
-const una = require('una-language')
-
-module.exports = (_, config) => ({
-    parserOverride: (code, options) =>
-        parser.parse(options.sourceFileName.endsWith('.una') ? una(code, config) : code, options)
-})
