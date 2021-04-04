118900aac49772a888301849eb2e4df7e3cf129c
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-04 22:55:46 +0300

    Continue refactoring

---
+++ src/transform.rules.js
@@ -0,0 +1,25 @@
+const pipeline = (children, orderFirst) => {
+    if (children.length === 1) return children
+    const [first, second, ...rest] = children
+    const application =
+        Array.isArray(second) && typeof second !== 'string'
+            ? orderFirst
+                ? [second[0], first, ...second.slice(1)]
+                : [...second, first]
+            : [second, first]
+    return pipeline([application, ...rest], orderFirst)
+}
+
+const func = (transform, value, children) => {
+    const parameters = Array.isArray(children[0]) ? children[0].map(transform) : children[0]
+    return [value, parameters, ...children.slice(1).map(transform)]
+}
+
+module.exports = {
+    '=': (transform, value, children) => [value, transform(children[0]), transform(children.slice(1))],
+    '<-=': (transform, value, children) => [value, transform(children)],
+    '->': func,
+    '-->': func,
+    '<|': (transform, value, children) => transform(pipeline(children, true)),
+    '|>': (transform, value, children) => transform(pipeline(children, false))
+}
