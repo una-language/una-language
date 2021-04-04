fb05f10cc9520bb6c5c9381e469de701bdf3bf24
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-04 23:46:55 +0300

    Working on translate refactoring

--- src/translate.js
+++ src/translate.js
@@ -1,7 +1,6 @@
-const translateRules = require('./translate.rules')
-
 module.exports = config => {
-    const expression = node => {
+    const translateRules = require('./translate.rules')(config)
+    const translate = node => {
         if (!Array.isArray(node)) {
             switch (node) {
                 case '::':
@@ -12,63 +11,19 @@ module.exports = config => {
                     return node
             }
         }
-        if (node.length === 1) return expression(node[0])
+        if (node.length === 1) return translate(node[0])
 
         const [value, ...children] = node
+        const translateRule = translateRules[value]
+        if (translateRule) return translateRule(translate, value, children)
 
         if (typeof value === 'string' && value.startsWith('.') && value.length > 1) {
-            const field = `${expression(children[0])}.${value.substring(1)}`
-            return children.length > 1 ? `${field}(${children.slice(1).map(expression).join(', ')})` : field
+            const field = `${translate(children[0])}.${value.substring(1)}`
+            return children.length > 1 ? `${field}(${children.slice(1).map(translate).join(', ')})` : field
         }
 
-        const translateRule = translateRules[value]
-        if (translateRule) return translateRule(expression, value, children)
-
-        switch (value) {
-            case '=->':
-                const isSingleImport = children.length < 2
-                switch (config.modules) {
-                    case 'import':
-                        return isSingleImport
-                            ? `import ${children[0]}`
-                            : `import ${children.slice(1).map(expression).join(', ')} from ${children[0]}`
-                    case 'require':
-                        return isSingleImport
-                            ? `require(${children[0]})`
-                            : `const ${children.slice(1).map(expression).join(', ')} = require(${children[0]})`
-                    default:
-                        throw new Error("Option 'modules' can be only 'import' or 'require'")
-                }
-            case '<-=':
-                const areChildrenArray = Array.isArray(children[0])
-                const exportType =
-                    Array.isArray(children[0]) && children[0][0] === '='
-                        ? 'const'
-                        : areChildrenArray && Array.isArray(children[0][0]) && children[0][0].length === 0
-                        ? 'common'
-                        : 'default'
-
-                const isES = config.modules === 'import'
-
-                switch (exportType) {
-                    case 'default':
-                        const defaultExportBody = expression(children[0])
-                        return isES ? `export default ${defaultExportBody}` : `module.exports = ${defaultExportBody}`
-                    case 'common':
-                        const exportBody = children[0].slice(1).map(expression).join(', ')
-                        return isES ? `export { ${exportBody} }` : `module.exports = { ${exportBody} }`
-                    case 'const':
-                        return isES
-                            ? `export ${expression(children[0])}`
-                            : `module.exports.${expression(children[0][1])} = ${expression(children[0].slice(2))}`
-                }
-
-            default:
-                return !!children && children.length > 0
-                    ? `${expression(value)}(${children.map(expression).join(', ')})`
-                    : node
-        }
+        return !!children && children.length > 0 ? `${translate(value)}(${children.map(translate).join(', ')})` : node
     }
 
-    return expression
+    return translate
 }
--- src/translate.rules.js
+++ src/translate.rules.js
@@ -8,23 +8,21 @@ const operatorMapping = {
 }
 
 const getOperator = value => operatorMapping[value] || value
-const unary = (expression, value, children) => `${getOperator(value)}${expression(children[0])}`
-const nary = (expression, value, children) => `(${children.map(expression).join(` ${getOperator(value)} `)})`
-const optionary = (expression, value, children) => (children.length > 1 ? nary : unary)(expression, value, children)
+const unary = (translate, value, children) => `${getOperator(value)}${translate(children[0])}`
+const nary = (translate, value, children) => `(${children.map(translate).join(` ${getOperator(value)} `)})`
+const optionary = (translate, value, children) => (children.length > 1 ? nary : unary)(translate, value, children)
 
-const funcBody = (expression, lines) =>
-    lines
-        .map((line, index) => (index === lines.length - 1 ? `return ${expression(line)}` : expression(line)))
-        .join('; ')
-const func = (expression, children) => {
+const funcBody = (translate, lines) =>
+    lines.map((line, index) => (index === lines.length - 1 ? `return ${translate(line)}` : translate(line))).join('; ')
+const func = (translate, children) => {
     const [paramsLine, ...lines] = children
-    const params = Array.isArray(paramsLine) ? paramsLine.map(expression).join(', ') : expression(paramsLine)
+    const params = Array.isArray(paramsLine) ? paramsLine.map(translate).join(', ') : translate(paramsLine)
     return lines.length === 1
-        ? `(${params}) => (${expression(lines[0])})`
-        : `(${params}) => { ${funcBody(expression, lines)} }`
+        ? `(${params}) => (${translate(lines[0])})`
+        : `(${params}) => { ${funcBody(translate, lines)} }`
 }
 
-module.exports = {
+module.exports = config => ({
     '!': unary,
     '!!': unary,
     '+': nary,
@@ -42,54 +40,93 @@ module.exports = {
     '!=': nary,
     '!~=': nary,
     '-': optionary,
-    '=': (expression, value, children) => `const ${expression(children[0])} = ${expression(children[1])}`,
-    '?': (expression, value, children) =>
-        `(${expression(children[0])} ? ${expression(children[1])} : ${
-            children.length > 2 ? expression(children[2]) : 'undefined'
+    '=': (translate, value, children) => `const ${translate(children[0])} = ${translate(children[1])}`,
+    '?': (translate, value, children) =>
+        `(${translate(children[0])} ? ${translate(children[1])} : ${
+            children.length > 2 ? translate(children[2]) : 'undefined'
         })`,
-    '?!': (expression, value, children) => {
+    '?!': (translate, value, children) => {
         const returnBodyLines = children.slice(1)
         const returnBody =
             returnBodyLines.length === 1
-                ? `return ${expression(returnBodyLines[0])}`
-                : `{ ${funcBody(expression, returnBodyLines)} }`
-        return `if (${expression(children[0])}) ${returnBody}`
+                ? `return ${translate(returnBodyLines[0])}`
+                : `{ ${funcBody(translate, returnBodyLines)} }`
+        return `if (${translate(children[0])}) ${returnBody}`
     },
-    '->': (expression, value, children) => func(expression, children),
-    '-->': (expression, value, children) => `async ${func(expression, children)}`,
-    '|->': (expression, value, children) => {
-        const tryBody = funcBody(expression, children[0].slice(1))
-        const catchBody = funcBody(expression, children[1].slice(2))
-        const tryCatch = `try { ${tryBody} } catch (${expression(children[1][1])}) { ${catchBody} }`
+    '->': (translate, value, children) => func(translate, children),
+    '-->': (translate, value, children) => `async ${func(translate, children)}`,
+    '|->': (translate, value, children) => {
+        const tryBody = funcBody(translate, children[0].slice(1))
+        const catchBody = funcBody(translate, children[1].slice(2))
+        const tryCatch = `try { ${tryBody} } catch (${translate(children[1][1])}) { ${catchBody} }`
 
         const isAsync = children[0][0] === '<--' || children[1][0] === '-->'
         return `${isAsync ? 'await (async ' : '('}() => { ${tryCatch} })()`
     },
-    '<-': (expression, value, children) => `(${func(expression, [[], ...children])})()`,
-    '<--': (expression, value, children) =>
+    '=->': (translate, value, children) => {
+        const isSingleImport = children.length < 2
+        switch (config.modules) {
+            case 'import':
+                return isSingleImport
+                    ? `import ${children[0]}`
+                    : `import ${children.slice(1).map(translate).join(', ')} from ${children[0]}`
+            case 'require':
+                return isSingleImport
+                    ? `require(${children[0]})`
+                    : `const ${children.slice(1).map(translate).join(', ')} = require(${children[0]})`
+            default:
+                throw new Error("Option 'modules' can be only 'import' or 'require'")
+        }
+    },
+    '<-': (translate, value, children) => `(${func(translate, [[], ...children])})()`,
+    '<--': (translate, value, children) =>
         children.length > 1
-            ? `await (async ${func(expression, [[], ...children])})()`
-            : `await ${expression(children[0])}`,
-    '<-|': (expression, value, children) => `(() => { throw new Error(${expression(children[0])}) })()`,
-    '::': (expression, value, children) => `[${children.map(expression).join(', ')}]`,
-    ':': (expression, value, children) =>
+            ? `await (async ${func(translate, [[], ...children])})()`
+            : `await ${translate(children[0])}`,
+    '<-|': (translate, value, children) => `(() => { throw new Error(${translate(children[0])}) })()`,
+    '<-=': (translate, value, children) => {
+        const areChildrenArray = Array.isArray(children[0])
+        const exportType =
+            Array.isArray(children[0]) && children[0][0] === '='
+                ? 'const'
+                : areChildrenArray && Array.isArray(children[0][0]) && children[0][0].length === 0
+                ? 'common'
+                : 'default'
+
+        const isES = config.modules === 'import'
+
+        switch (exportType) {
+            case 'default':
+                const defaultExportBody = translate(children[0])
+                return isES ? `export default ${defaultExportBody}` : `module.exports = ${defaultExportBody}`
+            case 'common':
+                const exportBody = children[0].slice(1).map(translate).join(', ')
+                return isES ? `export { ${exportBody} }` : `module.exports = { ${exportBody} }`
+            case 'const':
+                return isES
+                    ? `export ${translate(children[0])}`
+                    : `module.exports.${translate(children[0][1])} = ${translate(children[0].slice(2))}`
+        }
+    },
+    '::': (translate, value, children) => `[${children.map(translate).join(', ')}]`,
+    ':': (translate, value, children) =>
         `{${children
             .map(child =>
                 typeof child === 'string'
-                    ? expression(child)
+                    ? translate(child)
                     : child.length > 1
                     ? child[0] === '.'
-                        ? `[${expression(child[1])}]: ${expression(child.slice(2))}`
-                        : `${expression(child[0])}: ${expression(child.slice(1))}`
-                    : expression(child[0])
+                        ? `[${translate(child[1])}]: ${translate(child.slice(2))}`
+                        : `${translate(child[0])}: ${translate(child.slice(1))}`
+                    : translate(child[0])
             )
             .join(', ')}}`,
-    '.': (expression, value, children) => `${expression(children[0])}[${expression(children[1])}]`,
-    '`': (expression, value, children) => {
+    '.': (translate, value, children) => `${translate(children[0])}[${translate(children[1])}]`,
+    '`': (translate, value, children) => {
         const firstChild = Array.isArray(children[0]) ? children[0][0] : children[0]
         const hasIdentifier =
             typeof firstChild === 'string' && !firstChild.startsWith("'") && !firstChild.startsWith('"')
-        const identifier = hasIdentifier ? expression(children[0]) : ''
+        const identifier = hasIdentifier ? translate(children[0]) : ''
         const lines = children.slice(hasIdentifier ? 1 : 0)
 
         const interpolatedString = lines
@@ -101,7 +138,7 @@ module.exports = {
                     (accumulator, substitution, index) =>
                         accumulator.replace(
                             new RegExp(`(?<!\\\\)\\$\\{${index}\\}`, 'g'),
-                            `\${${expression(substitution)}}`
+                            `\${${translate(substitution)}}`
                         ),
                     string.substring(1, string.length - 1)
                 )
@@ -109,4 +146,4 @@ module.exports = {
             .join('\n')
         return `${identifier}\`${interpolatedString}\``
     }
-}
+})
