e5e85d17481cf99c4c10edacb25aba39045fa25e
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-07 15:05:24 +0300

    Move parse rules

--- src/phases/parse.js
+++ src/phases/parse.js
@@ -1,5 +1,5 @@
 const _ = require('lodash')
-const parser = require('parsimmon')
+const parser = require('./parse.rules')
 
 const convertTabs = lines => {
     if (lines.length === 0) return ''
@@ -14,47 +14,11 @@ const convertTabs = lines => {
     )
 }
 
-const language = parser.createLanguage({
-    expression: rules =>
-        parser.alt(
-            rules.stringSingleQuote,
-            rules.stringDoubleQuote,
-            rules.number,
-            rules.symbol,
-            rules.list
-        ),
-    symbol: () =>
-        parser.regexp(/[a-zA-Z_|=:+*/`.?<>%&~!-][=a-zA-Z0-9_=|:+*/`.?<&>%!~-]*/).desc('symbol'),
-    stringSingleQuote: () =>
-        parser
-            .regexp(/'((?:\\.|.)*?)'/, 1)
-            .map(string => `'${string}'`)
-            .desc('string'),
-    stringDoubleQuote: () =>
-        parser
-            .regexp(/"((?:\\.|.)*?)"/, 1)
-            .map(string => `"${string}"`)
-            .desc('string'),
-    number: () =>
-        parser
-            .regexp(/-?(0|[1-9][0-9]*)([.][0-9]+)?([eE][+-]?[0-9]+)?/)
-            .map(Number)
-            .desc('number'),
-    list: rules =>
-        rules.expression
-            .trim(parser.optWhitespace)
-            .many()
-            .wrap(parser.string('('), parser.string(')')),
-    file: rules => rules.expression.trim(parser.optWhitespace).many()
-})
-
-const parse = text => {
+module.exports = config => text => {
     if (!text.trim()) return []
 
     const lines = text.split('\n').filter(line => line.trim())
     const indentedLines = lines.map(line => ({ level: line.search(/\S/) / 2, value: line.trim() }))
     const code = convertTabs(indentedLines)
-    return language.file.tryParse(code)
+    return parser.document.tryParse(code)
 }
-
-module.exports = config => parse
