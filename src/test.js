9a88e8a9895800ac63db8f9837396ed957ac21ca
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-04-17 16:39:19 +0300

    Add e2e test

---
+++ src/test.js
@@ -0,0 +1,27 @@
+const una = require('./index')
+const testUna = (text, result) => expect(eval(una(text)).toString().trim()).toEqual(result.trim())
+
+test('=', () => {
+    testUna(
+        `
+= a 1
+a    
+`,
+        '1'
+    )
+})
+
+test('+', () => {
+    testUna('+ 2 3', '5')
+})
+
+test('->', () => {
+    testUna(
+        `
+= func -> x
+  + x 1 
+func 2
+    `,
+        '3'
+    )
+})
