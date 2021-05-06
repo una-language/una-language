4729c4283c30f5f7b333bfec8add7f25f926178f
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-05-06 17:56:32 +0300

    Add macros

--- src/phases/transform.js
+++ src/phases/transform.js
@@ -1,12 +1,16 @@
-const transformRules = require('./transform.rules')
+module.exports = config => {
+  const transformRules = {
+    ...require('./transform.rules'),
+    ...config.rules.transform
+  }
+  const transform = raw => {
+    if (!Array.isArray(raw)) return raw
+    if (raw.length === 0) return raw
+    if (raw.length === 1) return transform(raw[0])
 
-const transform = raw => {
-  if (!Array.isArray(raw)) return raw
-  if (raw.length === 0) return raw
-  if (raw.length === 1) return transform(raw[0])
+    const [value, ...children] = raw
+    return transformRules.hasOwnProperty(value) ? transformRules[value](transform, value, children) : raw.map(transform)
+  }
 
-  const [value, ...children] = raw
-  return transformRules.hasOwnProperty(value) ? transformRules[value](transform, value, children) : raw.map(transform)
+  return expressions => expressions.map(transform)
 }
-
-module.exports = config => expressions => expressions.map(transform)
