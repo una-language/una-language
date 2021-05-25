2abc626c31ad734f282af4659ed08ef45ec9c11b
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-05-25 16:55:54 +0300

    Refactor transform

--- src/phases/transform.js
+++ src/phases/transform.js
@@ -1,10 +1,11 @@
 module.exports = findOperator => {
   const transform = expression => {
     if (!Array.isArray(expression)) return expression
-    if (expression.length === 0) return expression
-    if (expression.length === 1) return transform(expression[0])
 
     const [operator, ...operands] = expression
+    if (!operator) return expression
+    if (operands.length === 0) return transform(operator)
+
     const transformOperator = findOperator(operator, 'transform')
     return transformOperator ? transformOperator(transform, operator, operands) : expression.map(transform)
   }
