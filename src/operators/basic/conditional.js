24e48a9b5c8591f318898dd644c3e7333477ad68
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-06-17 12:29:12 +0300

    Refactor conditional operators

--- src/operators/basic/conditional.js
+++ src/operators/basic/conditional.js
@@ -1,21 +1,27 @@
 module.exports = ({ translateFunctionBody, validateParametersCount }) => [
   {
     match: '?',
-    translate: (translate, operator, operands) =>
-      `(${translate(operands[0])} ? ${translate(operands[1])} : ${
-        operands.length > 2 ? translate(operands[2]) : 'undefined'
-      })`,
+    translate: (translate, operator, operands) => {
+      const condition = translate(operands[0])
+      const trueBranch = translate(operands[1])
+      const falsebranch = operands.length > 2 ? translate(operands[2]) : 'undefined'
+
+      return `(${condition} ? ${trueBranch} : ${falsebranch})`
+    },
     validate: validateParametersCount(2, 3)
   },
   {
     match: '?!',
     translate: (translate, operator, operands) => {
+      const condition = translate(operands[0])
+
       const returnBodyLines = operands.slice(1)
       const returnBody =
         returnBodyLines.length === 1
           ? `return ${translate(returnBodyLines[0])}`
           : `{ ${translateFunctionBody(translate, returnBodyLines)} }`
-      return `if (${translate(operands[0])}) ${returnBody}`
+
+      return `if (${condition}) ${returnBody}`
     },
     validate: validateParametersCount(2)
   }
