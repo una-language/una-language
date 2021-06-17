4118e7abe0fff479cec6a7518c51987642b48670
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2021-06-17 12:37:20 +0300

    Refactor map operator

--- src/operators/basic/data.js
+++ src/operators/basic/data.js
@@ -2,19 +2,19 @@ module.exports = () => [
   { match: '::', translate: (translate, operator, operands) => `[${operands.map(translate).join(', ')}]` },
   {
     match: ':',
-    translate: (translate, operator, operands) =>
-      `{${operands
-        .map(child =>
-          typeof child === 'string'
-            ? translate(child)
-            : child.length > 1
-            ? child[0] === '.'
-              ? `[${translate(child[1])}]: ${translate(child.slice(2))}`
-              : child[0] === '...'
-              ? translate(operands)
-              : `${translate(child[0])}: ${translate(child.slice(1))}`
-            : translate(child[0])
-        )
-        .join(', ')}}`
+    translate: (translate, operator, operands) => {
+      const translateOperand = child => {
+        if (typeof child === 'string') return translate(child)
+
+        const [key, ...value] = child
+        if (child.length === 1) return translate(key)
+
+        if (key === '.') return `[${translate(value[0])}]: ${translate(value.slice(1))}`
+        if (key === '...') return translate(operands)
+
+        return `${translate(key)}: ${translate(value)}`
+      }
+      return `{${operands.map(translateOperand).join(', ')}}`
+    }
   }
 ]
